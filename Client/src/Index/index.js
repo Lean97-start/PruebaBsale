cb1b3e23b3f9d06557872fbdae4b89e249433f2b
Lean97-start <leandro_mza008@hotmail.com>
2022-11-13 03:01:39 -0300

    add lazy loading

--- Client/src/Index/index.js
+++ Client/src/Index/index.js
@@ -3,21 +3,36 @@ import {getAllCategories, getAllProducts} from "../Request/request.js";
 
 //Función que inicializa toda la app
 async function init(){
-    $("titleResult").hide();
-    let allCategories = await getAllCategories();
-    let products = await getAllProducts();
-    if(allCategories.length){
-        allCategories.forEach(category => {
-            $(`<a id=${category.id} name=${category.name.toUpperCase()} class="categoryLink">${category.name.toUpperCase()}</a>`).appendTo(".list-group")});
-        if(products.length){
-            products.forEach(product => {
-                $(modelCardProduct(product)).appendTo("#containerProducts");
-            });
-        }
+    try {
+        let allCategories = await getAllCategories();
+        let products = await getAllProducts();
+        if(allCategories.length){
+            document.getElementById("containerProducts").style.display = "grid";
+            document.getElementById('divProductNotFound').style.display = "none";
+            document.getElementById('divWithoutProduct').style.display = "none";
+            document.getElementById('loadingGIF').style.display = "none";
+            allCategories.forEach(category => {
+                $(`<a id=${category.id} name=${category.name.toUpperCase()} class="categoryLink">${category.name.toUpperCase()}</a>`).appendTo(".list-group")});
+            if(products.length){
+                products.forEach(product => {
+                    $(modelCardProduct(product)).appendTo("#containerProducts");
+                });
+            }
+        } 
+    } catch (error) {
+        setTimeout(() => {
+            document.getElementById('loadingGIF').style.display = "none";
+            document.getElementById("containerProducts").style.display = "none";
+            document.getElementById('divProductNotFound').style.display = "none";
+            document.getElementById('divWithoutProduct').style.display = "block";
+            init();
+          }, "2000")    
+        // document.getElementById("containerProducts").style.display = "none";
+        // document.getElementById('divProductNotFound').style.display = "none";
+        // document.getElementById('divWithoutProduct').style.display = "none";
+        // document.getElementById('loadingGIF').style.display = "block";
     }
+    
 }
-$(window).load(function() {
-    $(".loader").fadeOut("slow");
-});
 
 document.addEventListener('load', init(), false); //Para realizar la inicializacion.
